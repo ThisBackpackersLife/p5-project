f86c73f753a5103fb4b1c64ef5e2f3a90f1075b6
Nicholas <crazedglitch@yahoo.com>
2023-06-14 16:12:52 -0500

    initial commit

---
+++ server/app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Standard library imports
+
+# Remote library imports
+from flask import Flask, make_response, jsonify, request
+from flask_restful import Resource
+
+# Local imports
+from config import app, db, api
+# from models import 
+# Views go here!
+
+@app.route('/')
+def home():
+    return ''
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
---
+++ server/models.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
+from flask import abort 
+
+from config import db
+
+# Models go here!
