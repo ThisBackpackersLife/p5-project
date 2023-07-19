cd3b45851554d61deed246546d970421c87cf14a
Nicholas <crazedglitch@yahoo.com>
2023-07-19 12:19:12 -0500

    Trip: Edit & Delete Buttons Created

--- client/src/components/TripCard.js
+++ client/src/components/TripCard.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Card, CardContent, Grid, Typography, Button } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 import { Link } from "react-router-dom";
 
-function TripCard({ trip, selectTripId  }) {
+function TripCard({ trip, selectTripId, deleteTrip  }) {
     
     let { id, name, start_date, end_date, accommodation, budget, notes, destinations, itineraries, activities } = trip
 
@@ -14,9 +16,33 @@ function TripCard({ trip, selectTripId  }) {
                     <Grid item sm={ 30 } md={ 30 }>
                         <Card variant="outlined" sx={{ mb: 2 }}>
                             <CardContent>
-                                <Typography variant="h5" component="div">
-                                    { name }
-                                </Typography>
+                                <Grid container>
+                                    <Grid item xs={ 2 } sm={ 10 }>
+                                        <div style={{ display: "flex", justifyContent: "space-between" }}>
+                                            {/* Edit Trip Button */}
+                                            <Button
+                                                variant="outlined"
+                                                startIcon={ <EditIcon /> }
+                                                onClick={ null }
+                                            >
+                                                Edit Trip
+                                            </Button>
+                                            {/* Delete Trip Button */}
+                                            <Button
+                                                variant="outlined"
+                                                startIcon={ <DeleteIcon /> }
+                                                onClick={ () => deleteTrip( id ) }
+                                            >
+                                                Delete Trip
+                                            </Button>
+                                        </div>
+                                    </Grid>
+                                    <Grid item xs={ 10 } sm={ 10 }>
+                                        <Typography variant="h5" component="div">
+                                            { name }
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
                                 {/* Display other trip information */}
                                 <div>
                                     <Typography component="p">
@@ -34,6 +60,7 @@ function TripCard({ trip, selectTripId  }) {
                                     <Typography component="p">
                                         Notes: { notes }
                                     </Typography>
+                                    {/* Destination links */}
                                     <Typography component="p">
                                         Destinations:{ " " }
                                         { destinations && destinations.length > 0 ? (
@@ -59,6 +86,7 @@ function TripCard({ trip, selectTripId  }) {
                                                 Add Destination
                                             </Button>
                                     </Typography>
+                                    {/* Itinerary links */}
                                     <Typography component="p">
                                         Itineraries:{ " " }
                                         { itineraries && itineraries.length > 0 ? (
@@ -76,6 +104,7 @@ function TripCard({ trip, selectTripId  }) {
                                             "No itineraries available."
                                         )}
                                     </Typography>
+                                    {/* Activity links */}
                                     <Typography component="p">
                                         Activities:{ " " }
                                         { activities && activities.length > 0 ? (
@@ -93,7 +122,6 @@ function TripCard({ trip, selectTripId  }) {
                                             "No activities available."
                                         )}
                                     </Typography>
-
                                 </div>
                             </CardContent>
                         </Card>
